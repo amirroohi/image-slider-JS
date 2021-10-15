d6e045c059bd4b9d4ed17a5cd7ec8f8647b337a9
amirroohi <amirroohi@yahoo.com>
2021-10-16 00:20:51 +0330

    parallax edition added

---
+++ app.js
@@ -0,0 +1,6 @@
+const imageElement = document.querySelector(".image");
+const textElement = document.querySelector(".text");
+
+imageElement.addEventListener("DOMContentLoaded", blurImage());
+
+function blurImage() {}
